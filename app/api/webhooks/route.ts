1de51a0f8f5883b861c30e0e0357bdd0136e88c9
Hitish Rao P <140695309+HitishRaoP@users.noreply.github.com>
2024-06-06 15:15:00 +0530

    Update route.ts

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -5,7 +5,7 @@ import { db } from '@/lib/db'
 
 export async function POST(req: Request) {
 
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET_PROD
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
